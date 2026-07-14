350982ce43ff26c10d4586b18d008e64c37228cf
ayxan <ayxan.20145@gmail.com>
2026-07-14 12:37:34 +0400

    print os

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use sysinfo::System;
+
+fn main() -> io::Result<()> {
+    let mut sys = System::new_all();
+    sys.refresh_all();
+
+    let info = os_info::get();
+    let os = info.os_type();
+
+    println!("OS: {}", os);
+
+    Ok(())
 }
