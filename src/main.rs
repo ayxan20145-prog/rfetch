736bec535143a0844d1c1318233e278f122e3a15
ayxan <ayxan.20145@gmail.com>
2026-07-14 12:51:03 +0400

    display kernel, host, cpu and cores

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
-use std::io;
 use sysinfo::System;
 
-fn main() -> io::Result<()> {
+fn main() {
     let mut sys = System::new_all();
     sys.refresh_all();
 
     let info = os_info::get();
+
     let os = info.os_type();
 
-    println!("OS: {}", os);
+    let kernel = System::kernel_version();
 
-    Ok(())
+    let host = System::host_name().unwrap_or_default();
+
+    let cpus = sys.cpus();
+    let cpu = cpus[0].brand();
+    let cores = cpus.len();
+
+    println!("OS: {}", os);
+    match kernel {
+        Some(version) => println!("Kernel: {}", version),
+        None => println!("Could not get kernel version"),
+    }
+    println!("Host: {}", host);
+    println!("CPU: {}", cpu);
+    println!("Cores: {}", cores);
 }
